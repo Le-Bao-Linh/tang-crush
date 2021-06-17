02d050060d8f6d5aad967c13561d3aaed102758f
Le-Bao-Linh <85984321+Le-Bao-Linh@users.noreply.github.com>
2021-06-17 11:11:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Nope',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu chứ',
+    reply: 'yêu anh nhất trên đời này',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'm bắt anh đợi hơi lâu rồi đấy em.',
+    messDesc: 'làm ngiu anh nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
